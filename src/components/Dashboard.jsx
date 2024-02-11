b71045c266e2c5dfac40b3d85b5dda0cbfbdcf73
spencerrazor@gmail.com <spencerrazor@gmail.com>
2024-02-11 00:09:40 -0800

    fixed formatting

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -32,26 +32,43 @@ export const Dashboard = () => {
 
 
 // App.jsx
-return (
-  <>
-    <Navbar />
-    <div className="pt-[72px] md:pt-[48px] w-full"> 
-      <div className='w-full flex flex-col'>
-        <div className='bg-white'>
-          <div className='flex flex-col items-center mx-auto w-3/4 h-full gap-8 p-8 pt-16'>
-            <Search serviceClick={handleServiceClick}  getInsurance={getInsurance}/>
+// return (
+//   <>
+//     <Navbar />
+//     <div className="pt-[72px] md:pt-[48px] w-full h-[calc(100vh - 48px)]"> 
+//     <div className="flex flex-col h-full">
+//           <div className='basis-1 flex flex-col items-center mx-auto w-3/4 h-full gap-8 p-8 pt-16'>
+//             <Search serviceClick={handleServiceClick}  getInsurance={getInsurance}/>
+//           </div>
+//         {searchMade ?
+//         <div className='h-[75vh] flex flex-col lg:flex-row w-full'>
+//           <Map selectedService={selectedService} selectedInsurance={selectedInsurance} getHospital={getHospital} />
+//           {selectedHospital && <Chart selectedHospital={selectedHospital} selectedService={selectedService} selectedInsurance={selectedInsurance}/>}
+//         </div>
+//         :
+//         <div className="h-full">hi</div>
+//         }
+//     </div>
+//     </div>
+//   </>
+// );
 
+return (
+    <div className="flex flex-col h-screen">
+    <Navbar />
+    <div className="flex-grow pt-[72px] md:pt-[48px] w-full overflow-auto">
+    <div className='basis-1 flex flex-col items-center mx-auto w-3/4 h-full gap-8 p-8 pt-16'>
+            <Search serviceClick={handleServiceClick}  getInsurance={getInsurance}/>
           </div>
-        </div>
-        {searchMade && 
+        {searchMade ?
         <div className='h-[75vh] flex flex-col lg:flex-row w-full'>
           <Map selectedService={selectedService} selectedInsurance={selectedInsurance} getHospital={getHospital} />
           {selectedHospital && <Chart selectedHospital={selectedHospital} selectedService={selectedService} selectedInsurance={selectedInsurance}/>}
         </div>
-        
+        :
+        <div className="h-full">hi</div>
         }
-      </div>
     </div>
-  </>
-);
+  </div>
+)
 };
